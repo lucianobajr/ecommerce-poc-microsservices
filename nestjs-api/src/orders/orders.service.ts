d56c7b1ac71be57ad2dc9d948b36cea440f215bc
lucianobajr <luciano.alcantara@ufv.br>
2024-01-31 18:43:14 -0300

    add(+): ready

--- nestjs-api/src/orders/orders.service.ts
+++ nestjs-api/src/orders/orders.service.ts
@@ -5,11 +5,14 @@ import { Order } from './entities/order.entity';
 import { In, Repository } from 'typeorm';
 import { Product } from '../products/entities/product.entity';
 
+import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
+
 @Injectable()
 export class OrdersService {
   constructor(
     @InjectRepository(Order) private orderRepo: Repository<Order>,
     @InjectRepository(Product) private productRepo: Repository<Product>,
+    private amqpConnection: AmqpConnection,
   ) {}
 
   async create(createOrderDto: CreateOrderDto & { client_id: number }) {
@@ -42,6 +45,12 @@ export class OrdersService {
     });
     await this.orderRepo.save(order);
 
+    await this.amqpConnection.publish('amq.direct', 'OrderCreated', {
+      order_id: order.id,
+      card_hash: createOrderDto.card_hash,
+      total: order.total,
+    });
+
     return order;
   }
 
@@ -59,7 +68,7 @@ export class OrdersService {
   findOne(id: string, client_id: number) {
     return this.orderRepo.findOneByOrFail({
       id,
-      client_id
+      client_id,
     });
   }
 }
